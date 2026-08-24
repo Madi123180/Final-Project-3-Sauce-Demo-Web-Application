6865bb6262a3e851510fb27c04a8b1d6d87c37b9
Madi123180 <malathibsc15@gmail.com>
2026-08-24 13:09:05 +0530

    Update Checkout.py

--- Pages/Checkout.py
+++ Pages/Checkout.py
@@ -17,22 +17,22 @@ class Checkout:
         self.finish_button="finish"
         self.wait = WebDriverWait(self.driver, 10)
 
-    def click_checkout_button(self): # function to click on bike light product
+    def click_checkout_button(self): 
         self.wait.until(EC.presence_of_element_located((By.ID, self.checkout_button))).click()
 
-    def enter_fname(self,firstname): # function to click on bike light product
+    def enter_fname(self,firstname): 
         self.wait.until(EC.presence_of_element_located((By.ID, self.fname))).send_keys(firstname)
 
-    def enter_lname(self,lastname): # function to click on bike light product
+    def enter_lname(self,lastname): 
         self.wait.until(EC.presence_of_element_located((By.ID, self.lname))).send_keys(lastname)
 
-    def enter_zip(self,postalcode): # function to click on bike light product
+    def enter_zip(self,postalcode):
         self.wait.until(EC.presence_of_element_located((By.ID, self.zip))).send_keys(postalcode)
 
-    def click_continue_button(self): # function to click on bike light product
+    def click_continue_button(self): 
         self.wait.until(EC.presence_of_element_located((By.ID, self.continue_button))).click()
 
-    def click_finish_button(self): # function to click on bike light product
+    def click_finish_button(self): 
         self.wait.until(EC.presence_of_element_located((By.ID, self.finish_button))).click()
 
 
